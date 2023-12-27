9e97d5fd3deef3c4c3ef1f4042bed5f9d3f64592
MD Rahi <fhrstation21@gmail.com>
2023-12-27 21:12:01 +0600

    add basic slider for slider 1

--- src/Components/Slider1/Slider1.jsx
+++ src/Components/Slider1/Slider1.jsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
+import { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft";
+import { FaArrowRight } from "@react-icons/all-files/fa/FaArrowRight";
 import ProductCard from "../Products/ProductCard";
 import Products from "../Products/Products";
 
 const Slider1 = () => {
-
     const { products } = Products();
+    const productsPerPage = 4;
+
+    const [startIndex, setStartIndex] = useState(0);
+
+    const handlePrev = () => {
+        setStartIndex((prevIndex) => Math.max(0, prevIndex - productsPerPage));
+    };
+
+    const handleNext = () => {
+        setStartIndex((prevIndex) =>
+            Math.min(prevIndex + productsPerPage, products.length - productsPerPage)
+        );
+    };
 
     return (
-        <>
-            <div className="w-full lg:w-5/6 grid grid-cols-1 lg:grid-cols-2 gap-16 p-4 justify-items-center mx-auto mt-12">
-                {
-                    products.map((product, index) => {
-                        return <ProductCard key={index} product={product} />
-                    })
-                }
+        <div className="w-full lg:w-5/6 mx-auto mt-12">
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-16 p-4 justify-items-center relative overflow-hidden">
+                <button
+                    onClick={handlePrev}
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 shadow-2xl p-3 rounded-full border-2 border-gray-100"
+                >
+                    <FaArrowLeft className="text-gray-600 text-2xl" />
+                </button>
+
+                {products.slice(startIndex, startIndex + productsPerPage).map((product, index) => (
+                    <div
+                        key={index + startIndex}
+                        className="col-span-1 lg:col-span-2"
+                    >
+                        <ProductCard product={product} />
+                    </div>
+                ))}
+
+                <button
+                    onClick={handleNext}
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2 shadow-2xl p-3 rounded-full border-2 border-gray-100"
+                >
+                    <FaArrowRight className="text-gray-600 text-2xl" />
+                </button>
             </div>
-        </>
+        </div>
     );
 };
 
