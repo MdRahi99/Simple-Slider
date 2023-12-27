a89f2153f8d8291bc06b93110719fcf61a48a52b
MD Rahi <fhrstation21@gmail.com>
2023-12-28 04:11:54 +0600

    ...

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Slider1 from '../Slider1/Slider1';
+import Slider2 from '../Slider2/Slider2';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Slider1 />
+            <Slider2 />
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Slider1 from "../Components/Slider1/Slider1";
 import Slider2 from "../Components/Slider2/Slider2";
 import Main from "../Layouts/Main";
+import Home from "../Components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "/slider1",
         element: <Slider1 />,
